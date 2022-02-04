398044cfd9b804f64300e2acf1277205566574f5
Matthew James <mjames4@woolworths.com.au>
2022-02-04 15:50:03 +1100

    Bootstrap testing project

---
+++ test/ConsumerService.Contracts/ConsumerServiceFixture.cs
@@ -0,0 +1,6 @@
+﻿namespace ConsumerService.Contracts
+{
+    internal class ConsumerServiceFixture
+    {
+    }
+}
---
+++ test/ConsumerService.Contracts/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace ConsumerService.Contracts
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
