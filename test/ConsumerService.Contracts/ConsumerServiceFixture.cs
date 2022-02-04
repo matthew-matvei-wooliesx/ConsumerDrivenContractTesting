c97d48e95b341fd6820a01677cd93f953a329011
Matthew James <mjames4@woolworths.com.au>
2022-02-04 16:44:27 +1100

    Implement consumer understanding of contract

--- test/ConsumerService.Contracts/ConsumerServiceFixture.cs
+++ test/ConsumerService.Contracts/ConsumerServiceFixture.cs
@@ -1,6 +1,23 @@
-﻿namespace ConsumerService.Contracts
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Wow.Pact.Consumer;
+
+namespace ConsumerService.Contracts
 {
-    internal class ConsumerServiceFixture
+    public class ConsumerServiceFixture : WowPactConsumerFixture
     {
+        public ConsumerServiceFixture() : base(Options)
+        {
+        }
+
+        private static WowPactConsumerFixtureOptions Options => new()
+        {
+            ConsumerName = "ConsumerDrivenContractTesting.ConsumerService",
+            ProviderName = "ConsumerDrivenContractTesting.UpstreamService",
+            JsonSerializerSettings = new JsonSerializerSettings
+            {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            }
+        };
     }
 }
